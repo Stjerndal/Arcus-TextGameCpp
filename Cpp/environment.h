37dc452ce362583b26baf377449ca7803be95d6d
Stjerndal <stjernd@kth.se>
2015-01-14 20:05:56 +0100

    Working on environment.

--- Cpp/environment.h
+++ Cpp/environment.h
@@ -5,17 +5,21 @@
 #include <vector>
 
 namespace arcus {
+	class Actor;
+	class Item;
+
 	enum direction_t {UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT, UP_LEFT};
+
 	class Environment {
 	public:	
 		const std::vector<direction_t> directions() const;
-		const Environment neighbor(direction_t direction) const;
-		void openDirection(direction_t direction);
+		const Environment neighbor(direction_t) const;
+		void openDirection(direction_t);
 		const std::string description() const;
-		void enter(Actor actor);
-		void leave(Actor actor);
-		void pick_up(/*Item*/);
-		void drop(/*Item*/);
+		void enter(Actor);
+		void leave(Actor);
+		void pick_up(Item);
+		void drop(Item);
 		void affect(Actor actor);
 	};
 
