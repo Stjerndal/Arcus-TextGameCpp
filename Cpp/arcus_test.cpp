b70c2aa42576792edd8cfdf76de056600ba130aa
Stjerndal <stjernd@kth.se>
2015-01-14 20:05:28 +0100

    Creating a first test class.

---
+++ Cpp/arcus_test.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+#include "actor.h"
+#include "environment.h"
+
+int main() {
+	std::cout << "flerp" << std::endl;
+	arcus::Actor actor();
+	arcus::Environment env();
+	return 0;
+}
