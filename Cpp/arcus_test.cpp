5e86a290f8b90dc051f07fda449358ba8a9f63cd
Stjerndal <stjernd@kth.se>
2015-01-15 23:29:10 +0100

    Adding to the Test Class.

--- Cpp/arcus_test.cpp
+++ Cpp/arcus_test.cpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
 
 #include "actor.h"
 #include "environment.h"
@@ -21,10 +24,40 @@
 #include "onoffitem.h"
 #include "indoor.h"
 #include "outdoor.h"
+#include "dialog.h"
 
 int main() {
 	std::cout << "flerp" << std::endl;
-	arcus::Actor actor();
-	arcus::Environment env();
+	arcus::Actor actor("Human", "Peter", 42);
+	std::cout << actor.getName() << std::endl;
+
+	arcus::Corporeal corpse("Orbis", "Klas", 50, "Red", 10, 10, 10, 100);
+	std::cout << corpse.getHp() << std::endl;
+
+	arcus::Item key("Key", 32, 2, 500, "Gold", "A golden Key");
+	std::cout << key.getDescription() << std::endl;
+
+	arcus::Item ball("Ball", 32, 2, 500, "Blue", "A soft blue ball");
+	std::cout << ball.getDescription() << std::endl;
+
+	arcus::Container bag("Backpack", 1, 1, 1327, "Brown", "A simple bag", 50, 10);
+	bag.add(key);
+	bag.add(key);
+	std::cout << bag.getHold_weight() << "kg, " << bag.getCur_weight() << "kg, " << std::endl;
+	std::cout << "is ball removable?" << bag.remove(ball) << std::endl;
+	std::cout << bag.getHold_weight() << "kg, " << bag.getCur_weight() << "kg, " << std::endl;
+	bag.remove(key);
+	std::cout << bag.getHold_weight() << "kg, " << bag.getCur_weight() << "kg, " << std::endl;
+	bag.remove(key);
+	std::cout << bag.getHold_weight() << "kg, " << bag.getCur_weight() << "kg, " << std::endl;
+
+	std::vector<arcus::Direction_t> directions = {arcus::UP, arcus::RIGHT, arcus::LEFT, arcus::DOWN};
+	arcus::Environment desert("A desert");
+
+	arcus::Environment forest("A forest");
+	forest.addNeighbor(desert, arcus::UP);
+	forest.openDirection(arcus::UP);
+	std::cout << forest.getDescription() << ", UP: " << forest.getNeighbor(arcus::UP).getDescription() << std::endl;
+
 	return 0;
 }
