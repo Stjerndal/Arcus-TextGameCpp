cff949d49ceb4d018551abd8e0efd580bee98c4e
Stjerndal <stjernd@kth.se>
2015-01-16 16:47:47 +0100

    Moved enum Directions_t to separate file.

---
+++ Cpp/directions.hpp
@@ -0,0 +1,9 @@
+#ifndef ARCUS_DIRECTIONS_H
+#define ARCUS_DIRECTIONS_H
+
+
+namespace arcus {
+	enum Direction_t {NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST};
+}
+
+#endif
