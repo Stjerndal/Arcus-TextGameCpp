06625c6ae4a73a0e3d42e912c00ed301c9ca13d2
Stjerndal <stjernd@kth.se>
2015-01-21 04:40:32 +0100

    Monster now using weak_ptr.

--- Cpp/monster.h
+++ Cpp/monster.h
@@ -2,22 +2,23 @@
 #define ARCUS_MONSTER_H
 
 #include <string>
+#include <memory>
 
 namespace arcus {
 	class Corporeal;
 	class Monster: public Corporeal {
 	public:
-		const Corporeal getMaster() const;
-		const std::string getEats() const;
-		const int getHunger() const;
+		std::weak_ptr<Corporeal> getMaster() const;
+		std::string getEats() const;
+		int getHunger() const;
 
 		void setHunger(int);
 		void setWisdom(int);
 
-		void subjectTo(Corporeal);
+		void subjectTo(std::weak_ptr<Corporeal>);
 
 	private:
-		Corporeal master;
+		std::weak_ptr<Corporeal> master;
 		std::string eats;
 		int hunger;
 	};
