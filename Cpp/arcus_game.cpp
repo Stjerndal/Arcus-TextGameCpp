b21ef10711cf54420dce59e57107b896e127d6af
Stjerndal <stjernd@kth.se>
2015-01-21 03:57:45 +0100

    Cleaned up arcus_game.cpp

--- Cpp/arcus_game.cpp
+++ Cpp/arcus_game.cpp
@@ -3,31 +3,10 @@
 #include "userinterface.h"
 #include "world.h"
 
-
 std::string msg = "What do you do?";
 
-
-
-void update(arcus::World& world) {
-	/*UserInterface::present(world.present());
-	UserInterface::present(msg);
-	std::vector<std::string> input;
-	input = UserInterface::fetchInput();
-	world.handleInput(input);*/
-	//UserInterface::present(input + " <- thats what you wrote!");
-}
-
-void start() {
-
-	arcus::World world;
-
-/*
-	while(1)
-		update(world);*/
-	world.start();
-
-}
-
 int main() {
-	start();	
+	arcus::World world;
+	world.start();
+	
 }
